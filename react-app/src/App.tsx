bec11679dd9f849151cff4bea0abc66c78f3fc92
Robert <robert.jones@cromwell.co.uk>
2023-05-03 14:18:08 +0100

    expense form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,15 +8,23 @@ import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
-const [expenses, setExpenses] = useState([
-  {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
-  {id: 2, description: 'bbb', amount: 10, category: 'Utilities'},
-  {id: 3, description: 'ccc', amount: 10, category: 'Utilities'},
-  {id: 4, description: 'ddd', amount: 10, category: 'Utilities'}
-])
- 
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   const [buttonType, setButtonType] = useState("btn btn-primary");
   const [alerted, setAlerted] = useState(false);
@@ -76,7 +84,18 @@ const [expenses, setExpenses] = useState([
       <Cart cartItems={cartItems} onClear={() => setCartItems([])} /> */}
       {/* <ExpandableText maxChars={100} /> */}
       {/* <Form /> */}
-      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}/>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
       {/* <Like
         onClick={() => {
           console.log("Clicked");
